afd00763c385cce1b03bc7a829192b75ede5ab7a
Novadson <novadsonthelus@gmail.com>
2022-05-25 09:19:26 -0300

    First commit bit

---
+++ NetWithAngularAndReact/TrainingAngular.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingAngular.Domain.Entities;
+
+namespace TrainingAngular.Data.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static ModelBuilder SeedData(this ModelBuilder modelBuilder)
+        {
+            /*INSERT USER BY DEFAULT IF NTO EXISTS*/
+            modelBuilder.Entity<Users>().HasData(
+                new Users { Id = new System.Guid("fa2a200a-4ad4-4b20-a568-46d50b137fc8"), Name = "Novadson Thelus", Email = "novadsonthelus@gmail.com" }
+                );
+            return modelBuilder;
+        }
+    }
+}
---
+++ NetWithAngularAndReact/TrainingAngular.Domain/Entities/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrainingAngular.Domain.Entities
+{
+    #region TABLE
+    [Table("users")]
+    public class Users
+    {
+        #region PROPERTIES
+        [Key]
+        [Column("user_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Column("user_name")]
+        [Required(ErrorMessage = "The email address is required")]
+        public string Name { get; set; }
+
+        [Column("user_email")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+        #endregion PROPERTIES
+    }
+    #endregion TABLE
+}
